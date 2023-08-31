5c034183efc2580f052da8a464984e2540b70c0a
allanbonjorno <allanprofessional@outlook.com>
2023-08-31 10:49:17 -0300

    Alteração Adicionada

--- BancoSantoAndre/Program.cs
+++ BancoSantoAndre/Program.cs
@@ -1,88 +1,142 @@
-﻿namespace BancoSantoAndre
+﻿using System;
+
+namespace BancoSantoAndre01
 {
-    internal class Program
+    public class Program
     {
+
         private static ConsoleKeyInfo consoleKeyInfo;
+
         static void Main(string[] args)
-
-
         {
-            int opcao = 0;
-            string? nome;
 
+            int opcao;
 
             while (true)
             {
-                Console.BackgroundColor = ConsoleColor.DarkMagenta;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("****************************************");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("                  BANCO                 ");
-                Console.WriteLine("               SANTO ANDRÉ              ");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("****************************************");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("      1 - Criar Conta                   ");
-                Console.WriteLine("      2 - Listar Todas as Contas        ");
-                Console.WriteLine("      3 - Buscar Conta por Número       ");
-                Console.WriteLine("      4 - Atualizar dados da conta      ");
-                Console.WriteLine("      5 - Apagar Conta                  ");
-                Console.WriteLine("      6 - Sacar                         ");
-                Console.WriteLine("      7 - Depositar                     ");
-                Console.WriteLine("      8 - Transferir Valor entre contas ");
-                Console.WriteLine("      9 - Sair da Conta                 ");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("****************************************");
-                Console.WriteLine("                                        ");
-                Console.WriteLine("      Entre com a opção desejada        ");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("*****************************************************");
+                Console.WriteLine("                                                     ");
+                Console.WriteLine("                  BANCO SANTO ANDRÉ                  ");
+                Console.WriteLine("                                                     ");
+                Console.WriteLine("*****************************************************");
+                Console.WriteLine("                                                     ");
+                Console.WriteLine("            1 - Criar Conta                          ");
+                Console.WriteLine("            2 - Listar todas as Contas               ");
+                Console.WriteLine("            3 - Buscar Conta por Numero              ");
+                Console.WriteLine("            4 - Atualizar Dados da Conta             ");
+                Console.WriteLine("            5 - Apagar Conta                         ");
+                Console.WriteLine("            6 - Sacar                                ");
+                Console.WriteLine("            7 - Depositar                            ");
+                Console.WriteLine("            8 - Transferir valores entre Contas      ");
+                Console.WriteLine("            9 - Sair                                 ");
+                Console.WriteLine("                                                     ");
+                Console.WriteLine("*****************************************************");
+                Console.WriteLine("Entre com a opção desejada:                          ");
+                Console.WriteLine("                                                     ");
+                Console.ResetColor();
+
 
                 opcao = Convert.ToInt32(Console.ReadLine());
+
                 if (opcao == 9)
                 {
-                    
-                    Console.WriteLine("Você saiu!");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+                    Sobre();
+                    Console.ResetColor();
                     System.Environment.Exit(0);
                 }
+
                 switch (opcao)
                 {
                     case 1:
-                        Console.WriteLine("Seu nome completo:");
-                        nome = Convert.ToString(Console.ReadLine());
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Criar Conta\n\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                     case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Listar todas as Contas\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
                         break;
                     case 3:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Consultar dados da Conta - por número\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
                         break;
                     case 4:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Atualizar dados da Conta\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
                         break;
                     case 5:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Apagar a Conta\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
                         break;
                     case 6:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Saque\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
                         break;
                     case 7:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Depósito\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
                         break;
                     case 8:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Transferência entre Contas\n\n");
+                        Console.ResetColor();
+
+                        KeyPress();
                         break;
-
                     default:
-                        Console.WriteLine("Opção Inválida!");
-                        Console.WriteLine("Gostaria de voltar ao menu");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nOpção Inválida!\n");
+                        Console.ResetColor();
 
+                        KeyPress();
                         break;
                 }
-
-
-
-
-
-
-
-
             }
         }
+
+        static void Sobre()
+        {
+            Console.WriteLine("\n*********************************************************");
+            Console.WriteLine("Projeto Desenvolvido por: ");
+            Console.WriteLine("Generation Brasil - generation@generation.org");
+            Console.WriteLine("github.com/conteudoGeneration");
+            Console.WriteLine("*********************************************************");
+
+        }
+
+        static void KeyPress()
+        {
+            do
+            {
+                Console.Write("\nPressione Enter para Continuar...");
+                consoleKeyInfo = Console.ReadKey();
+            } while (consoleKeyInfo.Key != ConsoleKey.Enter);
+        }
+
     }
 }
